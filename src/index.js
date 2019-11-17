fedcc6a3b8b26f0533168816f5114660c5664dda
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-17 13:57:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -25,7 +25,14 @@ export class OlderCoaster extends Component {
 }
 
 export class InFrontOfYou extends Component {
-  
+  render() {
+    return (
+      <div>
+        <p>You shouldn't look too far.</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+      )
+  }
 }
 
 export class ButcherShop extends Component {
